b272828188b2f312046956cf00d959388bbf5f2e
Jason Kadrmas <blackjk@gmail.com>
2012-07-24 21:55:20 -0500

    Added jaws add command

--- jaws.js
+++ jaws.js
@@ -1,20 +1,13 @@
-var fs    = require('fs'),
+var q = require('q'),
+  fs = require('fs'),
   sys = require('util'),
   path = require('path'),
-  cssom = require('cssom'),
-  jsdom = require('jsdom'),
   async = require('async'),
-  https = require('https'),
-  http = require('http'),
-  urlLib = require('url'),
-  events = require('events'),
-  wrench = require('wrench'),
-  eventEmitter = new events.EventEmitter(),
-  _     = require('underscore'),
   commands = require('./lib/commands'),
+  github = require('./lib/github'),
+  bundler = require('./lib/bundler'),
   jaws_root = __dirname,
-  root = process.cwd(),
-  vendorPath = "js/vendor/";
+  root = process.cwd();
 
 commands.root = root;
 commands.jaws_root = __dirname;
@@ -33,97 +26,26 @@ function readFile( path, cb ) {
     });
 }
 
-function downloadBundle( url, path, key, version ) {
-  var self = this;
-  var parts = urlLib.parse(url);
-  var protocol = parts.protocol === 'https:' ? https : http;
-  var writeStream = fs.createWriteStream( path );
+// function download( url, path ) {
+//   var parts = urlLib.parse(url);
+//   var protocol = parts.protocol === 'https:' ? https : http;
+//   var writeStream = fs.createWriteStream( path );
 
-  protocol.get(parts, function (response) {
-    try {
-      if (response.statusCode === 200) {
-        response.pipe( writeStream );
+//   protocol.get(parts, function (response) {
+//     try {
+//       if (response.statusCode === 200) {
+//         response.pipe( writeStream );
 
-        writeStream.on('close', function () {
-          var versionStr = version ? ' (' + version + ')' : '';
-          sys.puts (" * Using " + key + versionStr);
-        });
-      }
-    } catch (e) {
-      console.error("Could not download file: ", url);
-    }
-  });
-}
-
-function getRepositories() {
-  var url = 'https://raw.github.com/blackjk3/jaws/master/repos/repos.json';
-  var self = this;
-  //var writeStream = fs.createWriteStream( 'js/vendor/testing.js' );
-  var parts = urlLib.parse(url);
-
-  https.get(parts, function (response) {
-
-    try {
-      if (response.statusCode === 200) {
-
-        var data = '';
-
-        response.on('data', function (chunk){
-            data += chunk;
-        });
-
-        response.on('end',function(){
-            var obj = JSON.parse(data);
-            eventEmitter.emit('repo', obj);
-        });
-      }
-    } catch (e) {
-      // d.reject(e);
-    }
-  });
-}
-
-function bundle() {
-  var packagePath = process.cwd() + '/package.json',
-      repos;
-
-  readFile( packagePath, function( data ) {
-    repos = JSON.parse(data).jaws.dependencies;
-    getRepositories();
-  });
-
-  eventEmitter.on('repo', function( repoList ) {
-    var repoJSON = repoList.repositories;
-    processDependencies(repos, repoJSON);
-  });
-}
-
-function refreshVendorFolder() {
-  wrench.rmdirSyncRecursive(vendorPath);
-  fs.mkdirSync( vendorPath, 0755 );
-}
-
-function processDependencies( repos, repoList ) {
-
-  // Refresh vendor
-  refreshVendorFolder();
-
-  _.each(repos, function(version, key) {
-    path.exists(vendorPath + key + '.js', function(exists) {
-      var url, path;
-      
-      if(!exists) {
-        if( repoList[key] ) {
-          url = repoList[key].url.replace('{version}', version);
-          path = repoList[key].versioned ? key + '-' + version + '.js' : key + '.js';
-          downloadBundle( url, vendorPath + path, key, version );
-        } else {
-          sys.puts (" * Could not find library " + key + '. Please add the path manually to package.json');
-        }
-      }
-    });
-  });
-}
+//         writeStream.on('close', function () {
+//           var versionStr = version ? ' (' + version + ')' : '';
+//           sys.puts (" * Added to " + key + versionStr);
+//         });
+//       }
+//     } catch (e) {
+//       console.error("Could not download file: ", url);
+//     }
+//   });
+// }
 
 /**
  * watchProject()
@@ -149,6 +71,7 @@ module.exports = {
           .option('g, --generate <framework>:<type> <name>', 'Generates a new model or view.')
           .option('watch, --watch <path>', 'Watches project for recompile changes.')
           .option('bundle, --bundle', 'Updates dependencies based on package.json file.')
+          .option('add, --add', 'Adds a dependency into the current directory.')
           .parse(process.argv);
         
         if ( process.argv.length === 2 ) {
@@ -170,7 +93,27 @@ module.exports = {
             watchProject();
           }
           else if ( program.bundle ) {
-            bundle();
+            bundler.bundle();
+          }
+          else if ( program.add ) {
+            var key = program.args[0];
+
+            bundler.getRepoList().then(function(repoList) {
+              var results = bundler.searchRepoList( key, repoList.repositories );
+              if( results ) {
+                if( results.versioned ) {
+                  github.latestTag( results.github ).then(function (lastestTag) {
+                    var url = results.url.replace('{version}', lastestTag),
+                        path = results.versioned ? key + '-' + lastestTag + '.js' : key + '.js';
+                    
+                    bundler.download( url, root + '/' + path );
+                    sys.puts(" Downloading " + key + " to " + root);
+                  });
+                }
+              } else {
+                sys.puts (" * Could not find library " + key + '.  Please contact @itooamaneatguy to get this repo added.  Or submit a pull request to https://github.com/blackjk3/jaws.');
+              }
+            });
           }
         }
     }
